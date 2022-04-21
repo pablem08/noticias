d69e836620c19086eb57baae81a412e6785b25ff
Pablo <pablem08@gmail.com>
2022-04-21 00:14:21 -0300

    Comenzando proyecto Noticias

---
+++ src/components/Formulario.js
@@ -0,0 +1,37 @@
+import styles from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
+const Formulario = () => {
+
+    const OPCIONES = [
+        { value: 'general', label: 'General' },
+        { value: 'business', label: 'Negocios' },
+        { value: 'entertainment', label: 'Entretenimiento' },
+        { value: 'health', label: 'Salud' },
+        { value: 'science', label: 'Ciencia' },
+        { value: 'sports', label: 'Deportes' },
+        { value: 'technology', label: 'Tecnología' }
+    ]
+
+    // Utilizo el custom hook
+    const [categoria, SelectNoticias] = useSelect('general', OPCIONES);
+
+    return (
+        <div className={`${styles.buscador} row`}>
+            <div className="col s12 m8 offset-n2">
+                <form>
+                    <h2 className={styles.heading}>Encuentra Noticias por Categoria</h2>
+                    <SelectNoticias />
+                    <div className="input-field col s12">
+                        <input
+                            type="submit"
+                            className={`${styles['btn-block']} btn-large amber darken-2`}
+                            value="Buscar"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Formulario;
